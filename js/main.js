df7c8f6ce9a0e082e153457492bc311b35b3eb0c
Luca <lucagoudon@yahoo.fr>
2025-10-24 09:33:46 +0200

    Ajout de toutes les pages et structure entière de la page HTML

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// On importe les fonctions (modules)
+// Les chemins indiquent où se trouvent les fichiers
+import philo from './philo.js'
+
+gsap.registerPlugin(ScrollTrigger)
+// On exécute les fonctions pour lancer les animations
+philo()
+
---
+++ js/philo.js
@@ -0,0 +1,24 @@
+// Code obligatoire
+export default function philo() {
+    // on recupere tout les calss/id
+  const masqueVideoPhilo = document.querySelector('#masque-video-philo')
+  const conteneurPhilo = document.querySelector('#conteneur-philo')
+  //on ajoute cette ligne
+
+
+  // DEBUT DE L'ANIMATION
+
+            
+            gsap.from(masqueVideoPhilo,{
+
+                scrollTrigger:{
+                    scroller:"body",
+                    trigger:conteneurPhilo,
+                   
+                    scrub:2,
+                    end:"top 10%"
+                },
+                duration:3,
+                width:0
+            })
+}
