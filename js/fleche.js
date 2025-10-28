d2a0ac36af01b46cd3f27c418e6f44210e39d50f
Luca <lucagoudon@yahoo.fr>
2025-10-28 11:49:06 +0100

    Avancement juste avant de jardin d'exception

---
+++ js/fleche.js
@@ -0,0 +1,48 @@
+export default function fleche(){
+    
+document.addEventListener("DOMContentLoaded", (event) => {
+          
+            gsap.set("#chevron", {rotate:(100)})
+            gsap.to("#chevron",{
+                
+                motionPath:{
+                    path: "#fleche",
+                    align: "#fleche",
+                    alignOrigin: [0.5, 1],
+                    autoRotate: -90
+                    
+                },
+                duration:3,
+               
+                scrollTrigger: {
+
+                    trigger: "#section-video-animation",
+                    start: "top 60%",
+                    
+                    scrub:2
+
+                }
+            })
+
+            
+
+            gsap.from("#fleche",{
+
+                drawSVG:1,
+                duration:3,
+                  scrollTrigger: {
+
+                    trigger: "#section-video-animation",
+                    start: "top 60%",
+                 
+                    scrub: 2
+
+                }
+
+            })
+
+
+
+        });
+
+    }
--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 // Les chemins indiquent où se trouvent les fichiers
 import philo from './philo.js'
 import carousel from './carousel.js'
+import fleche from './fleche.js'
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger,MotionPathPlugin,DrawSVGPlugin)
 // On exécute les fonctions pour lancer les animations
 philo()
 carousel()
+fleche()
