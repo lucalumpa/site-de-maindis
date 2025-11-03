93c628d7b855e4a6b130abd3a247b57f53ece662
Luca <lucagoudon@yahoo.fr>
2025-11-03 19:40:50 +0100

    Avavncé majeuredans le responsive, format tablette pret

--- js/carousel.js
+++ js/carousel.js
@@ -4,8 +4,14 @@ export default function carousel(){
         new Splide('#image-carousel', {
             type: 'loop',
             perPage: 3,
+             breakpoints: {
+                1025: {
+                perPage: 2,
+            },
+            },
             autoplay: true,
             gap: '1rem',
+           
         }).mount();
     });
   }
