ef80df3df9a21bbb9521901cb8fbaec889b4ae22
Luca <lucagoudon@yahoo.fr>
2025-11-07 12:28:56 +0100

    cahrgement de la vidéo popup au moment de l'ouverture uniquement

--- js/popup-box-video.js
+++ js/popup-box-video.js
@@ -4,11 +4,17 @@ export default function popup_box_video (){
     const videoPopupOverlay = document.querySelector("#video-popup-overlay")
     const svgCroix = document.querySelector("#svg-croix")
     const overlay = document.querySelector("#overlay")
+    let video = document.querySelector("#video-popup-overlay video")
 
     boxPlay.addEventListener("click", popUp);
     svgCroix.addEventListener("click", closePopup);
 
     function popUp() {
+
+        video.src = video.dataset.src;
+        video.load();
+        video.play();
+
         videoPopupOverlay.style.visibility = "visible";
         overlay.style.visibility = "visible";
         videoPopupOverlay.style.opacity = "100";
@@ -22,6 +28,9 @@ export default function popup_box_video (){
         videoPopupOverlay.style.opacity = "0";
         overlay.style.visibility = "hidden";
         overlay.style.opacity = "0";
+            video.pause();    // arrêt propre
+    video.currentTime = 0; // rewind
+
 
     }
 }
